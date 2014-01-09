75df52d7e17ec4ae3d6c85d918234cf12e8a66d0
Jonathan Vollebregt <jonathan-vola@hotmail.com>
2014-01-09 20:19:51 +0100

    Remove forum selectbox for normal users

--- template.php
+++ template.php
@@ -25,7 +25,8 @@ function whitestier_menu_local_tasks_alter(&$data, $router_item, $root_path) {
 // If we're at the forum base or in a container remove the new forum post link
   if( $root_path == "forum" ||
       (
-	$root_path == "forum/%" && isset($router_item['page_arguments'][0]->container)
+        $root_path == "forum/%" &&
+        isset($router_item['page_arguments'][0]->container)
       )
     ){
       unset($data['actions']['output']['forum']);
@@ -38,4 +39,23 @@ function whitestier_menu_local_tasks_alter(&$data, $router_item, $root_path) {
 function whitestier_preprocess_node(&$vars){
   $vars['submitted'] = t('published by !username on !datetime',
         array('!username' => $vars['name'], '!datetime' => $vars['date']));
+}
+
+function whitestier_form_alter(&$form, &$form_state, $form_id) {
+  if($form_id == 'forum_node_form') {
+    // Hide forum selectbox from normal users (For use with access controls
+    // like taxonomy_access_control)
+    if(!user_access("administer forums")){
+      // Set default forum value on new forum post to url
+      if( array(arg(0),arg(1),arg(2)) == array("node", "add", "forum") &&
+          is_numeric(arg(3))
+        ){
+        $form['taxonomy_forums']['und']['#default_value'][0] = arg(3);
+      }
+      
+      // Remove shadow checkbox and forum selectbox
+      $form['shadow']['#type'] = "hidden";
+      $form['taxonomy_forums']['und']['#type'] = "hidden";
+    }
+  }
 }
